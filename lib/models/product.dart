0c7b4060fe66652cd093aca5f73284ab11657c1e
ngocanh <nguyenngocanh2695@gmail.com>
2026-03-14 23:34:15 +0700

    Init: Cau truc du an, Models, CartProvider base

---
+++ lib/models/product.dart
@@ -0,0 +1,30 @@
+// lib/models/product.dart
+class Product {
+  final int id;
+  final String title;
+  final double price;
+  final String description;
+  final String category;
+  final String image;
+
+  Product({
+    required this.id,
+    required this.title,
+    required this.price,
+    required this.description,
+    required this.category,
+    required this.image,
+  });
+
+  // Chuyển từ JSON (API) sang Object Flutter
+  factory Product.fromJson(Map<String, dynamic> json) {
+    return Product(
+      id: json['id'],
+      title: json['title'],
+      price: json['price'].toDouble(),
+      description: json['description'],
+      category: json['category'],
+      image: json['image'],
+    );
+  }
+}
